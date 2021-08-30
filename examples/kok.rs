c57b0b242f3331e862c338a385a2c62cfdefbf31
coral <jonas@jonasbengtson.se>
2021-08-29 21:55:35 -0700

    MOTULUL

--- examples/kok.rs
+++ examples/kok.rs
@@ -6,11 +6,15 @@ use std::time::Duration;
 async fn main() {
     let ip = Ipv4Addr::from_str("10.0.1.5").unwrap();
     let mut mt = motu::MOTU::new(ip);
-    mt.connect().await;
+    mt.connect().await.unwrap();
 
-    //tokio::time::sleep(Duration::from_secs(2)).await;
+    mt.set("ext/obank/0/ch/0/stereoTrim", -45).await.unwrap();
 
-    mt.set("ext/obank/0/ch/0/stereoTrim", -48).await.unwrap();
-
-    tokio::time::sleep(Duration::from_secs(2)).await;
+    let myval = mt
+        .get("ext/obank/0/ch/0/stereoTrim")
+        .await
+        .unwrap()
+        .as_f64()
+        .unwrap();
+    dbg!(myval);
 }
--- src/motu.rs
+++ src/motu.rs
@@ -3,12 +3,12 @@ use rand::Rng;
 use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::fmt::Display;
-use std::hash::Hash;
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::Ipv4Addr;
 use std::ops::DerefMut;
 use std::sync::Arc;
 use std::time::Duration;
 use thiserror::Error;
+use tokio::sync::oneshot;
 use tokio::sync::Mutex;
 use tokio::time;
 
@@ -21,6 +21,7 @@ pub enum MOTUError {
     RequestError(#[from] reqwest::Error),
 }
 
+#[allow(dead_code)]
 pub struct MOTU {
     addr: Ipv4Addr,
     client_id: u32,
@@ -48,33 +49,48 @@ impl MOTU {
     }
 
     pub async fn connect(&mut self) -> Result<(), MOTUError> {
+        let data =
+            MOTU::get_cache(self.client.clone(), self.url.clone(), self.cache.clone()).await?;
+
+        let cacheref = self.cache.clone();
+        *cacheref.lock().await.deref_mut() = data;
+
         let cacheref = self.cache.clone();
         let url = self.url.clone();
         let client = self.client.clone();
-
         tokio::spawn(async move {
             let mut interval = time::interval(Duration::from_secs(10));
             loop {
                 interval.tick().await;
 
-                let resp = match client.get(&url).send().await {
-                    Ok(v) => v,
-                    Err(e) => {
-                        error!("{}", e);
-                        continue;
-                    }
-                };
-
-                match resp.json::<HashMap<String, Value>>().await {
+                match MOTU::get_cache(client.clone(), url.clone(), cacheref.clone()).await {
                     Ok(data) => {
                         *cacheref.lock().await.deref_mut() = data;
                     }
+
                     Err(_) => {}
                 }
             }
         });
 
         Ok(())
+    }
+
+    async fn get_cache(
+        client: reqwest::Client,
+        url: String,
+        cacheref: Arc<Mutex<HashMap<String, Value>>>,
+    ) -> Result<HashMap<String, Value>, MOTUError> {
+        let resp = client.get(&url).send().await?;
+
+        Ok(resp.json::<HashMap<String, Value>>().await?)
+    }
+
+    pub async fn get(&self, key: &str) -> Option<Value> {
+        match self.cache.lock().await.get(&key.to_string()) {
+            Some(v) => Some(v.clone()),
+            None => None,
+        }
     }
 
     pub async fn set<T: Display + serde::ser::Serialize>(
@@ -84,14 +100,13 @@ impl MOTU {
     ) -> Result<reqwest::Response, MOTUError> {
         let url = self.url.clone();
 
-        dbg!(format!("{{\"{}\": {}}}", key, value));
-
         let form =
             reqwest::multipart::Form::new().text("json", format!("{{\"{}\": {}}}", key, value));
 
         let res = self.client.patch(url).multipart(form).send().await?;
 
-        self.cache
+        let ok = self
+            .cache
             .lock()
             .await
             .insert(key.to_string(), json!(value));
